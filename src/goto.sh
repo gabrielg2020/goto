c1abe3a6ab44108b58ee02637e8e6a9376013800
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-20 19:08:04 +0000

    refactor: comment code
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- src/goto.sh
+++ src/goto.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 function goto() {
+    # Check argument
     if [ -z "$1" ]; then
         echo "Usage: goto <directory_name_pattern>"
         return 1
     fi
+
+    # Setup local variables
     local pattern="$1"
     local dir
+
+    # Find directory and pipe to fzf
     dir=$(find . -type d -iname '*' -print 2>/dev/null \
         | fzf --query="$patten" --exit-0 --height 40% --reverse --prompt="Goto> ")
+
+    # Change directory
     if [ -n "$dir" ]; then
         cd "$dir" || echo "Error: Cannot change directory to $dir"
     else
