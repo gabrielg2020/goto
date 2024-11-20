601fd85feea166b7e0ca35fa1ce0a1c5065dce4e
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-20 19:03:13 +0000

    fix: fix script...

--- src/goto.sh
+++ src/goto.sh
@@ -8,7 +8,7 @@ function goto() {
     local pattern="$1"
     local dir
     dir=$(find . -type d -iname '*' -print 2>/dev/null \
-        | fzf --query="$patte --height 40% --reverse --prompt="Goto> ")
+        | fzf --query="$patten" --height 40% --reverse --prompt="Goto> ")
     if [ -n "$dir" ]; then
         cd "$dir"
     else
