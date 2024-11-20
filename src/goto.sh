d9e6a0918c88f3d0a43bb6152e7a0827582fd75b
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2024-11-20 19:18:50 +0000

    fix: spelling mistake

--- src/goto.sh
+++ src/goto.sh
@@ -8,7 +8,7 @@ function goto() {
     local pattern="$1"
     local dir
     dir=$(find . -type d -iname '*' -print 2>/dev/null \
-        | fzf --query="$patten" --height 40% --reverse --prompt="Goto> ")
+        | fzf --query="$pattern" --height 40% --reverse --prompt="Goto> ")
     if [ -n "$dir" ]; then
         cd "$dir"
     else
